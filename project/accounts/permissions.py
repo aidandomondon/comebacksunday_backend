ef89e84cba418d07a321710877679adbbafa114f
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-07-21 18:42:41 -0400

    Remove unused permission classes

--- project/accounts/permissions.py
+++ project/accounts/permissions.py
@@ -23,14 +23,7 @@ class FollowRequestPermission(permissions.BasePermission):
             return current_user == obj.followee
         else:
             return False    # unsupported action
-        
-    class IsParty(permissions.BasePermission):
-        """
-        Grants permission to the parties (follower or followee) 
-        of a `Follow` or `FollowRequest`.
-        """
-        def has_object_permission(self, request, view, obj):
-            return super().has_object_permission(request, view, obj)
+
             
 class FollowPermission(permissions.BasePermission):
     """
@@ -46,34 +39,3 @@ class FollowPermission(permissions.BasePermission):
             return current_user == obj.follower or current_user == obj.followee
         else:
             return False    # unsupported action
-    
-class IsOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
-    
-class IsFollower(permissions.BasePermission):
-    """
-    Custom permission to only allow followers of a post's author to view it.
-    """
-    def has_object_permission(self, request, view, obj):
-        return obj.owner in request.user.following
-    
-class CanEditPost(permissions.BasePermission):
-    """
-    Custom permission to only allow the author of a post to edit it.
-    """
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner
-    
-class CanReadPost(permissions.BasePermission):
-    """
-    Custom permission to allow followers of a post's author to view it.
-    """
-    def has_object_permission(self, request, view, obj):
-        is_follower = IsFollower.has_object_permission(self, request, view, obj)
-        is_owner = IsOwner.has_object_permission(self, request, view, obj)
-        return request.method == "GET" and (is_follower or is_owner)
